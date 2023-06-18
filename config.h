7064df154f075273cd42aebaf6fd71521e71e144
Paulo Henrique <paulo13579691@gmail.com>
2023-06-18 14:06:10 -0300

    new color scheme

--- config.h
+++ config.h
@@ -6,23 +6,31 @@
 #define BROWSER "firefox"
 /* appearance */
 static const unsigned int borderpx  = 3;        /* border pixel of windows */
-static const unsigned int gappx     = 12;        /* gaps between windows */
+static const unsigned int gappx     = 3;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const int horizpadbar        = 6;        /* horizontal padding for statusbar */
-static const int vertpadbar         = 8;        /* vertical padding for statusbar */
-static const char *fonts[]          = { "Ubuntu:weight=bold:size=8:antialas=true:hinting=true", "JoyPixels:size=12:antialias=true:autohint=true","monospace:size=10" }; //ttf-joypixels, ttf-ubuntu-font-family
+static const int horizpadbar        = 12;        /* horizontal padding for statusbar */
+static const int vertpadbar         = 12;        /* vertical padding for statusbar */
+static const char *fonts[]          = { "Ubuntu:weight=bold:size=11:antialas=true:hinting=true"}; //ttf-joypixels, ttf-ubuntu-font-family
 																					
+//static const char norm_fg[] = "#ff6e67";
+//static const char norm_bg[] = "#282a36";
+//static const char norm_border[] = "#dfdfdd";
+//
+//static const char sel_fg[] = "#bfbfbf";
+//static const char sel_bg[] = "#282a36";
+//static const char sel_border[] = "#bfbfbf";
 
-static const char norm_fg[] = "#ff6e67";
-static const char norm_bg[] = "#282a36";
-static const char norm_border[] = "#dfdfdd";
 
-static const char sel_fg[] = "#bfbfbf";
-static const char sel_bg[] = "#282a36";
-static const char sel_border[] = "#bfbfbf";
+static const char norm_bg[] = "#222222";
+static const char norm_fg[] = "#bbbbbb";
+static const char norm_border[] = "#444444";
+
+static const char sel_bg[] = "#005577";
+static const char sel_fg[] = "#eeeeee";
+static const char sel_border[] = "#770000";
 
 static const char urg_fg[] = "#f7f7fb";
 static const char urg_bg[] = "#5af78e";
@@ -32,30 +40,32 @@ static const char title_fg[] = "#8AFF80";
 static const char title_bg[] = "#282a36";
 static const char col_borderbar[]   = "#bfbfbf";
 
+
 static const char *colors[][3]      = {
     /*               fg           bg         border                         */
     [SchemeNorm] = { norm_fg,     norm_bg,   norm_border }, // unfocused wins
     [SchemeSel]  = { sel_fg,      sel_bg,    sel_border },  // the focused win
 };
-static const char dmenufont[]       = "monospace:size=10";
+static const char dmenufont[]       = "Ubuntu:weight=bold:size=8:antialas=true:hinting=true";
 static const char col_1[]       = "#b80f0f"; //background color of bar
 static const char col_2[]       = "#444444"; //border color unfocesed windows
 static const char col_3[]       = "#bbbbbb";
 static const char col_4[]       = "#eeeeee"; //border color focused windows and tags
 /* tagging */
-static const char *tags[] = { "work1", "work2", "free", "4", "5"};
+static const char *tags[] = { "1", "2", "3", "4", "5"};
 
 static const Rule rules[] = {
 	/* xprop(1):
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class     instance  title           tags mask  isfloating  isterminal  noswallow  monitor */
-	{ "Gimp",    NULL,     NULL,           0,         1,          0,           0,        -1 },
-	{ "Firefox", NULL,     NULL,           1 << 8,    0,          0,          -1,        -1 },
-	{ "St",      NULL,     NULL,           0,         0,          1,           0,        -1 },
-	{ "kitty",      NULL,     NULL,           0,         0,          1,           0,        -1 },
-	{ NULL,      NULL,     "Event Tester", 0,         0,          0,           1,        -1 }, /* xev */
+	/* class     instance  title                   tags mask  isfloating  isterminal  noswallow  monitor */
+	{ "Gimp",               NULL,     NULL,           0,         1,          0,           0,        -1 },
+	{ "Navigator",            NULL,     NULL,           0,         0,          0,           -1,        -1 },
+	{ "virtualbox",         NULL,     NULL,           0,         0,          0,           -1,        -1 },
+	{ "St",                 NULL,     NULL,           0,         0,          1,           0,        -1 },
+	{ "Kitty",              NULL,     NULL,           0,         0,          1,           0,        -1 },
+	{ NULL,                 NULL,     "Event Tester", 0,         0,          0,           1,        -1 }, /* xev */
 };
 
 /* layout(s) */
@@ -87,14 +97,16 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_1, "-nf", col_3, "-sb", col_3, "-sf", col_4, NULL};
 static const char *termcmd[]  = { TERMINAL, NULL };
 static const char *musiccmd[]  = { "spotify", NULL };
+static const char *notescmd[] = {TERMINAL, "-e", "nvim", "todolist"};
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_space,  spawn,          {.v = dmenucmd } },
 	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
-	{ MODKEY,                       XK_s, spawn,               {.v = musiccmd } },
+	{ MODKEY,                       XK_s,      spawn,          {.v = musiccmd } },
   { MODKEY,			                  XK_r,		   spawn,		       {.v = (const char*[]){ TERMINAL, "-e", "lfub", NULL } } },
-        { MODKEY,			XK_w,		spawn,		{.v = (const char*[]){ BROWSER, NULL } } },
+  { MODKEY,			                  XK_w,		   spawn,		       {.v = (const char*[]){ BROWSER, NULL } } },
+  { MODKEY,			                  XK_n,		   spawn,		       {.v = notescmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
